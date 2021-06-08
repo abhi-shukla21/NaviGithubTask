b486349989ee4a0245773a50cdc86bd297a9b25a
abhi-shukla21 <shukla.abhishek833@gmail.com>
2021-06-08 22:14:50 +0530

    First commit

---
+++ app/src/main/java/com/example/navigithubtask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.navigithubtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
